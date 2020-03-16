69ae7c1c4c00c1d45db5402d5121d3dccb84897f
Mimi Flynn <mimiflynn@gmail.com>
2020-03-16 10:58:26 -0400

    add install script for bash settings

---
+++ shared/bash/install.sh
@@ -0,0 +1,3 @@
+curl -o .bash_prompt https://raw.githubusercontent.com/mimiflynn/dotfiles/master/shared/bash/.bash_prompt
+curl -o .bash_profile https://raw.githubusercontent.com/mimiflynn/dotfiles/master/shared/bash/.bash_profile
+source .bash_profile
